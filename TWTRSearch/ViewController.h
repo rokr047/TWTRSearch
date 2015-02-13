7abc7fbc242ae5d938fd2f27406511f90db15c4e
Karthikeyan Sreenivasan <rokr047@gmail.com>
2015-02-12 20:42:20 -0800

    Initial Commit

---
+++ TWTRSearch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TWTRSearch
+//
+//  Created by Karthikeyan Sreenivasan on 2/12/15.
+//  Copyright (c) 2015 Karthikeyan Sreenivasan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TWTRSearch/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TWTRSearch
+//
+//  Created by Karthikeyan Sreenivasan on 2/12/15.
+//  Copyright (c) 2015 Karthikeyan Sreenivasan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
